e0cc142631c6cf13335f595d3bde76f553a12450
Raul Martin <alu0100769579@ull.edu.es>
2017-11-22 18:02:26 +0000

    actualizando rutas comprobando si el usuario esta registrado #7

--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,45 @@
-"use strict";
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('login.ejs', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
+
+router.get('/inicio', isLoggedIn, function(req, res) {
+  res.render('index.ejs', { user: req.user });
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/inicio',
+  failureRedirect: '/signup',
+  failureFlash: true,
+}));
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/inicio',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
 
 
-module.exports = function(app) {
-	
-	app.get('/', function(req, res) {
-		res.render('login.ejs');
-	});
-	
-	
-	app.get('/inicio', function(req, res) {
-		res.render('index.ejs');
-	});
-};
+module.exports = router;
 
-
-
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+  res.redirect('/');
+}
