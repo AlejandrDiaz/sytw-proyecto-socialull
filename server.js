06a07167903fb74afde45f6a234549886f4c4de9
Raul Martin <alu0100769579@ull.edu.es>
2017-11-22 18:04:21 +0000

    arreglando cosas del servidor #1

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
-"use strict";
+var express = require('express');
+var path = require('path');
+//var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-let express = require('express');
-let app = express();
-let session = require('express-session');
+var port = process.env.PORT || 3000;
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var session = require('express-session');
+
+var routes = require('./app/routes');
+//var users = require('./app/users');
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
 
-app.set('port', (process.env.PORT || 8080));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ secret: 'shhsecret', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport')(passport);
+
+app.use('/', routes);
+//app.use('/users', users);
+
+app.listen(port);
 
 
-require('./app/routes.js')(app);
-
-
-var server = app.listen(app.get('port'), function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-});
+module.exports = app;
