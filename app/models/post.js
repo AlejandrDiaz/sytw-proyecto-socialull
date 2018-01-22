83452172eef0b56f9170eeaa1c79bd634885c2e5
Raul Martin <alu0100769579@ull.edu.es>
2018-01-22 04:31:15 +0000

    Actualizando schema post y user con todos los elementos #9 #11

--- app/models/post.js
+++ app/models/post.js
@@ -6,10 +6,10 @@ var Schema = mongoose.Schema;
 var PostsSchema = mongoose.Schema({
     estado : String,                                                             
     file: String,
-    owner: { type: Schema.Types.ObjectId, ref: 'User' }
+    type: String,
+    owner: { type: Schema.Types.ObjectId, ref: 'User' },
+    likes: { type:Number, default: 0},
+    user_likes: [{type: Schema.Types.ObjectId ,ref: 'User'}]
 });
-
-//likes : Number                                                    //numero de likes
-//user_likes: [{type: Schema.Types.ObjectId ,ref: 'User'}]          //Personas que le han dado like
 
 module.exports = mongoose.model('Post', PostsSchema);
--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
-
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
+
+var Schema = mongoose.Schema;
 
 var userSchema = mongoose.Schema({
   local: {
@@ -8,12 +9,15 @@ var userSchema = mongoose.Schema({
     surname: String,
     edad: Number,
     email: String,
-    password: String
+    password: String,
+    estadociv: String,
+    genero: String,
+    grado: String,
+    biografia: String,
+    avatar: { type: String , default: "images/avatar.png"},
+    friends: [{type: Schema.Types.ObjectId , ref: 'User'}]
   }
 });
-
-//friends: [{type: Schema.Types.ObjectId ,ref: 'User'}]     //Personas que son amigos del usuario
-//se puede agregar algún tipo de estado de solicitud, algo como solicitud pendiente,aceptado, ...
 
 userSchema.methods.generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
@@ -24,37 +28,3 @@ userSchema.methods.validPassword = function(password) {
 };
 
 module.exports = mongoose.model('User', userSchema);
-
-
-
-
-
-
-/*
-'use strict';
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
-
-var UserSchema = mongoose.Schema({
-    local: {
-        name: String,
-        surname: String,
-        email: String,
-        password: String
-    }
-});
-
-// métodos ======================
-// generar un hash a partir de la contraseña
-UserSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// verificar si la contraseña es válida
-UserSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
-
-// exportar el modelo
-module.exports = mongoose.model('User', UserSchema);
-*/
